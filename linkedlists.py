30d05d3c777f922e66145546a0a80cd8fe995811
mnickey <mnickey@gmail.com>
2018-04-03 17:06:04 -0700

    Linked list implementation

--- linkedlists.py
+++ linkedlists.py
@@ -7,12 +7,14 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # method for printing the linked list
     def print_list(self):
         cur_node = self.head
         while cur_node:
             print(cur_node.data)
             cur_node = cur_node.next
 
+    # appending to the linked list
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -23,11 +25,13 @@ class LinkedList:
             last_node = last_node.next
         last_node.next = new_node
 
+    # appending before a node in a linked list
     def prepend(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
+    # appending after a node in a linked list
     def insert_after_node(self, prev_node, data):
         if not prev_node:
             print("Previous node is not in the list")
@@ -36,6 +40,7 @@ class LinkedList:
         new_node.next = prev_node.next
         prev_node.next = new_node
 
+    # deleting a node in a linked list
     def delete_node(self, key):
         cur_node = self.head
         if cur_node and cur_node.data == key:
@@ -51,6 +56,7 @@ class LinkedList:
         prev.next = cur_node.next
         cur_node = None
 
+    # node deletion by position
     def delete_node_at_pos(self, pos):
         cur_node = self.head
         if pos == 0:
